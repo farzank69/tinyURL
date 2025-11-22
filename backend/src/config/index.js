cfe701c74af40d3c5510ef5ccf64cf29ff17b534
Farzan Khan <farzankhan61r@gmail.com>
2025-11-22 21:37:53 +0530

    update env

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -5,5 +5,5 @@ module.exports = {
   baseUrl: process.env.BASE_URL || `http://localhost:${process.env.PORT || 3000}`,
   nodeEnv: process.env.NODE_ENV || 'development',
   databaseUrl: process.env.DATABASE_URL,
-  clientOrigin: process.env.VITE_CLIENT_ORIGIN, // Use VITE_CLIENT_ORIGIN from global .env
+  clientOrigin: process.env.VITE_CLIENT_ORIGIN,
 };

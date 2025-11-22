a9f09f74361bb5d2af9d20ac53c013488311431c
Farzan Khan <farzankhan61r@gmail.com>
2025-11-22 13:59:41 +0530

    update env for production

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '..', '..', '.env') });
 
 module.exports = {
   port: process.env.PORT || 3000,
   baseUrl: process.env.BASE_URL || `http://localhost:${process.env.PORT || 3000}`,
   nodeEnv: process.env.NODE_ENV || 'development',
   databaseUrl: process.env.DATABASE_URL,
-  clientOrigin: process.env.CLIENT_ORIGIN, // Add CLIENT_ORIGIN to config
+  clientOrigin: process.env.VITE_CLIENT_ORIGIN, // Use VITE_CLIENT_ORIGIN from global .env
 };
